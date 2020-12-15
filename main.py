74c8e24004be119fd4027e38bb1101af76b52fe2
Ricardo Dimas <rhenrique.dimas@gmail.com>
2020-12-14 23:41:34 -0300

    Main program skeleton

---
+++ main.py
@@ -0,0 +1,12 @@
+##################################
+# Basik-Calc
+# Calculator for basic operations
+# Author: Ricardo Dimas
+##################################
+
+def main():
+    print('Welcome to Basik-Calc\n')
+
+if __name__ == '__main__':
+    main()
+
