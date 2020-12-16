e4a7dd68c9f50f40adeb6808edbad9909a835f2a
Ricardo Dimas <rhenrique.dimas@gmail.com>
2020-12-16 08:16:13 -0300

    initial version

--- main.py
+++ main.py
@@ -4,22 +4,8 @@
 # Author: Ricardo Dimas
 ##################################
 
-def sum(a, b):
-    print(int(a) + int(b))
-
 def main():
     print('Welcome to Basik-Calc\n')
-    operation = input("""Which operation would you like to perform?
-    (1) Addition;
-    (2) Subtraction;
-    (3) Multiplication;
-    (4) Division;\n""")
-
-    if int(operation) == 1:
-        a = input('Type in the first number: ')
-        b = input('Type in the second number: ')
-        sum(a, b)
-        
 
 if __name__ == '__main__':
     main()
