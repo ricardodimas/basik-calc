864768bd50aa31ba8946af71c52ade85213a1917
Ricardo Dimas <rhenrique.dimas@gmail.com>
2020-12-16 08:29:51 -0300

    basic operations added

--- main.py
+++ main.py
@@ -7,6 +7,19 @@
 def sum(a, b):
     print(int(a) + int(b))
 
+def sub(a, b):
+    print(int(a) - int(b))
+
+def mult(a, b):
+    print(int(a) * int(b))
+
+def div(a, b):
+    print(int(a) / int(b))
+
+#def basic_text():
+#    a = input('Type in the first number: ')
+#    b = input('Type in the second number: ')
+
 def main():
     print('Welcome to Basik-Calc\n')
     operation = input("""Which operation would you like to perform?
@@ -19,7 +32,21 @@ def main():
         a = input('Type in the first number: ')
         b = input('Type in the second number: ')
         sum(a, b)
-        
+
+    if int(operation) == 2:
+        a = input('Type in the first number: ')
+        b = input('Type in the second number: ')
+        sub(a, b)
+
+    if int(operation) == 3:
+        a = input('Type in the first number: ')
+        b = input('Type in the second number: ')
+        mult(a, b)
+
+    if int(operation) == 4:
+        a = input('Type in the first number: ')
+        b = input('Type in the second number: ')
+        div(a, b)
 
 if __name__ == '__main__':
     main()
